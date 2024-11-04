9dd9e54b09e5500d9274d626560e38a8d12a2b43
유현 <yh7119@gmail.com>
2024-11-04 19:55:47 +0900

    관심 상품 조회 API 구현

--- src/main/java/com/study/myselectshop/controller/ProductController.java
+++ src/main/java/com/study/myselectshop/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.study.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -22,6 +24,11 @@ public class ProductController {
     @PutMapping("/products/{id}")
     public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         return productService.updateProduct(id, requestDto);
+    }
+
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+        return productService.getProducts();
     }
 
 }
